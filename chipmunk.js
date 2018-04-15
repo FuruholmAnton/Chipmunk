07747d2ed98281132b6d01d931a1227cc5d113ba
Anton Furuholm <anton.furuholm@gmail.com>
2018-04-15 17:19:49 +0200

    Make possible to parse HTML string directly

--- chipmunk.js
+++ chipmunk.js
@@ -1,16 +1,24 @@
 /**
  * A simple template rendering function
  * 
- * @param {*} ref - Either a DOM reference or a string selector
+ * @param {*} ref - Either a DOM reference or a string class or ID selector or a HTML string
  * @param {*} data - An object containing the values to replace with
  * @return new HTML string with replaced values
  */
 export default function render(ref, data) {
-    // Get the element if ref is a string
-    if (typeof ref == 'string') ref = document.querySelector(ref);
+    let tplString;
 
+    // Get the element if ref is a string selector
     // Stores the HTML as a string
-    let tplString = ref.innerHTML;
+    if (typeof ref == 'string') {
+        const firstChar = ref.slice(0,1);
+        if (firstChar == '.' || firstChar == '#') {
+            ref = document.querySelector(ref);
+        } else {
+            tplString = ref;
+        }
+    } 
+    if (!tplString) tplString = ref.innerHTML
 
     // Replaces all instances of brackets with coresponding value in the data object and then return
     return tplString.replace(/\{{(.*?)\}}/g, (match) => {

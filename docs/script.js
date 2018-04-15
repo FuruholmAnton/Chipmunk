822b77d4e05983fbbc726745f459791e1b77935f
Anton Furuholm <anton.furuholm@gmail.com>
2018-04-15 15:07:35 +0200

    nesting example

--- docs/script.js
+++ docs/script.js
@@ -19,7 +19,18 @@ const newHTML = render('#tpl', {
     text: 'Success!',
     span: 'This text is rendered by JS.',
     obj: {
-        complex: 'Nesting also works',
+        complex: 'Nesting also ',
+        nest: {
+            as: {
+                deep: {
+                    as: {
+                        you: {
+                            want: 'works',
+                        }
+                    }
+                }
+            }
+        }
     }
 })
 document.body.innerHTML += newHTML;
